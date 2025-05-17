95be605705c2640a8c2a3ad45269f9a6a64065b9
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-05-16 18:03:28 -0700

    Added Basic Setup for Controllers

---
+++ src/main/java/com/blogplatform/blogapi/controller/AuthorController.java
@@ -0,0 +1,13 @@
+package com.blogplatform.blogapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/author")
+public class AuthorController {
+
+
+
+
+}
---
+++ src/main/java/com/blogplatform/blogapi/controller/PostController.java
@@ -0,0 +1,9 @@
+package com.blogplatform.blogapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/post")
+public class PostController {
+}
