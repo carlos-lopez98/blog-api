d7aa69c9b22dc49172cf9857d291c60a210fd39d
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-05-16 18:41:49 -0700

    AuthorController Implemented

--- src/main/java/com/blogplatform/blogapi/controller/AuthorController.java
+++ src/main/java/com/blogplatform/blogapi/controller/AuthorController.java
@@ -1,49 +1,59 @@
 package com.blogplatform.blogapi.controller;
 
 import com.blogplatform.blogapi.dto.AuthorDTO;
-import com.blogplatform.blogapi.repository.AuthorRepository;
-import com.blogplatform.blogapi.service.AuthorServiceImpl;
+import com.blogplatform.blogapi.service.AuthorService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.method.annotation.RequestHeaderMapMethodArgumentResolver;
 
 import java.util.List;
 
+//TODO: @ControllerAdvice + @ExceptionHandler
+//TODO: Implement Custom Exceptions - AuthorNotFoundException
 @RestController
-@RequestMapping("/author")
+@RequestMapping("/authors")
 public class AuthorController {
 
     @Autowired
-    private AuthorServiceImpl authorService;
-
+    private AuthorService authorService;
 
     @PostMapping
-    public ResponseEntity createAuthor(@RequestBody AuthorDTO dto){
+    public ResponseEntity<AuthorDTO> createAuthor(@RequestBody AuthorDTO dto){
 
         AuthorDTO created = authorService.createAuthor(dto);
 
-        return ResponseEntity.ok("Succesfully created" + created.getName());
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
-    @GetMapping("/id")
-    public ResponseEntity<AuthorDTO> getById(@PathVariable Long id){
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id){
 
-        return ResponseEntity.ok(authorService.getAuthorById(id));
+        AuthorDTO retrieved;
+
+        try{
+            retrieved =  authorService.getAuthorById(id);
+            return ResponseEntity.ok(retrieved);
+        } catch(EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ID: " + id + "not found");
+        }
     }
 
-    @GetMapping("/all")
+    @GetMapping
     public ResponseEntity<List<AuthorDTO>> getAllAuthors(){
 
         return ResponseEntity.ok(authorService.getAllAuthors());
     }
 
-    @DeleteMapping("/id")
-    public ResponseEntity deleteById(@PathVariable Long id){
-        authorService.deleteAuthor(id);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
 
-        return ResponseEntity.ok("Author Successfully Deleted");
+        try{
+            authorService.deleteAuthor(id);
+            return ResponseEntity.ok("Author Successfully Deleted");
+        }catch(EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ID: " + id + " not found");
+        }
     }
 }
